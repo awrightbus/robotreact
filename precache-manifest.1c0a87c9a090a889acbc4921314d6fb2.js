ea5e68a7f16ef23d7ad4c8b208def2cebb1eb6a9
awright <awrightbus@gmail.com>
2020-07-11 22:42:28 -0500

    Updates

---
+++ precache-manifest.1c0a87c9a090a889acbc4921314d6fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e715c9021f97f9cc927507a00e42c60e",
+    "url": "/robotreact/index.html"
+  },
+  {
+    "revision": "b006326887f98c6d8944",
+    "url": "/robotreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c583a747143d36ae0b3f",
+    "url": "/robotreact/static/js/2.9e6ebbf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotreact/static/js/2.9e6ebbf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b006326887f98c6d8944",
+    "url": "/robotreact/static/js/main.876553d0.chunk.js"
+  },
+  {
+    "revision": "fc5ba97608e217295e6e",
+    "url": "/robotreact/static/js/runtime-main.ec2c8a71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robotreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
